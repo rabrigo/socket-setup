02f226fa816e7e54cbfd52c189289ae1f00c8e83
Raw Ness <rawness@Raws-Mac-mini.local>
2022-03-06 16:45:56 -0800

    completed setup with socket and react

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
-import Chat from './components/Chat'
+import React, { useState } from 'react';
+import io from "socket.io-client";
+const socket = io("http://localhost:4000");
 
 function App() {
+  const [text, setText] = useState('');
+
+  // sending to the server
+  socket.emit("emitting to server", "What's up?! From the client.");
+
+  // receiving from the server
+  socket.on("emitting to client", (textFromServer) => {
+    setText(textFromServer);
+  })
+
   return (
-    <div>
-      <div className="App">
-        <h1>Welcome to Browser Party</h1>
-        <Chat />
-      </div>
+    <div className="App">
+      <h1>Check your IDE's console for a message from the client.</h1>
+      {/* below should be displaying the message emitted from the server */}
+      <h2>{text}</h2>
     </div>
   );
-}
+};
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// if we don't run this we get a CORS error
+app.use(cors());
+
+// for now, take this as boilerplate
+const theServer = createServer();
+const io = new Server(theServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+});
 
 io.on('connection', socket => {
-  console.log(socket.id)
+  // when a user connects
+  console.log("You are now connected. This socket ID is unique everytime: " + socket.id);
+
+  // to confirm that the this is connected to the client, we'll send this message
+  socket.emit("emitting to client", "Hello from the server!")
+
+  // this is looking out for socket.emit("emitting to server") from the client side
+  socket.on("emitting to server", (textFromClient) => {
+    console.log(textFromClient);
+  })
 })
 
-http.listen(4000, function() {
+// need to ask how this redirects react server to port 4000
+theServer.listen(4000, function () {
   console.log('listening on port 4000')
 })
